856c67ca400b147c0e405369a881ccfbd7748326
djmikelp <djmikelp@gmail.com>
2020-07-27 18:55:42 -0400

    Pregunta 2

--- pregunta2/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ pregunta2/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -7,12 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Imaging;
 
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
         Bitmap bmp;
+        Bitmap tmpbmp;
         public Form1()
         {
             InitializeComponent();
@@ -23,6 +25,7 @@ namespace WindowsFormsApplication1
             openFileDialog1.Filter = "Archivos BMP|*.bmp|Archivos JPG|*.jpg|Todos los archivos|*.*";
             openFileDialog1.ShowDialog();
             bmp = new Bitmap(openFileDialog1.FileName);
+            tmpbmp = bmp;
             pictureBox1.Image = bmp;
             
         }
@@ -43,14 +46,77 @@ namespace WindowsFormsApplication1
                     d.SetPixel(i, x, nc);
                 }
             }
-            pictureBox1.Image = d;
-            */
-            //System.Drawing.Graphics gr = System.Drawing.Graphics.FromImage(bmp);
-           
-        }
-        //
-        
+             */
+            //pictureBox1.Image = d;
+            Bitmap image = bmp;
 
-        //
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    Color f = image.GetPixel(x, y);
+                    int luma = (int)(f.R * 0.3 + f.G * 0.59 + f.B * 0.11);
+                    image.SetPixel(x, y, Color.FromArgb(luma, luma, luma));
+                }
+            }
+            contornos(image, 5);
+            pictureBox1.Image = image;
+            
+        }
+        private static Bitmap contornos(Bitmap Image, float Value)
+        {
+            Value = (100.0f + Value) / 100.0f;
+            Value *= Value;
+            Bitmap NewBitmap = (Bitmap)Image.Clone();
+            BitmapData data = NewBitmap.LockBits(
+                new Rectangle(0, 0, NewBitmap.Width, NewBitmap.Height),
+                ImageLockMode.ReadWrite,
+                NewBitmap.PixelFormat);
+            int Height = NewBitmap.Height;
+            int Width = NewBitmap.Width;
+
+            unsafe
+            {
+                for (int y = 0; y < Height; ++y)
+                {
+                    byte* row = (byte*)data.Scan0 + (y * data.Stride);
+                    int columnOffset = 0;
+                    for (int x = 0; x < Width; ++x)
+                    {
+                        byte B = row[columnOffset];
+                        byte G = row[columnOffset + 1];
+                        byte R = row[columnOffset + 2];
+
+                        float Red = R / 255.0f;
+                        float Green = G / 255.0f;
+                        float Blue = B / 255.0f;
+                        Red = (((Red - 0.5f) * Value) + 0.5f) * 255.0f;
+                        Green = (((Green - 0.5f) * Value) + 0.5f) * 255.0f;
+                        Blue = (((Blue - 0.5f) * Value) + 0.5f) * 255.0f;
+
+                        int iR = (int)Red;
+                        iR = iR > 255 ? 255 : iR;
+                        iR = iR < 0 ? 0 : iR;
+                        int iG = (int)Green;
+                        iG = iG > 255 ? 255 : iG;
+                        iG = iG < 0 ? 0 : iG;
+                        int iB = (int)Blue;
+                        iB = iB > 255 ? 255 : iB;
+                        iB = iB < 0 ? 0 : iB;
+
+                        row[columnOffset] = (byte)iB;
+                        row[columnOffset + 1] = (byte)iG;
+                        row[columnOffset + 2] = (byte)iR;
+
+                        columnOffset += 4;
+                    }
+                }
+            }
+
+            NewBitmap.UnlockBits(data);
+
+            return NewBitmap;
+        }
+       
     }
 }
